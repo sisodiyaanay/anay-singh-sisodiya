9f12e3838d945bc0485fa2aa704f74ffe85f2cc5
sisodiyaanay <sisodiyaanay@iitgn.ac.in>
2024-05-20 23:47:36 +0530

    enhanced the darkmode toggle

--- anay-portfolio/src/components/darkmode/ThemeToggle.jsx
+++ anay-portfolio/src/components/darkmode/ThemeToggle.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './themetoggle.css';
 
 const ThemeToggle = () => {
+  // Function to handle theme toggling and reload the page
   const handleThemeToggle = () => {
-    document.body.classList.toggle('dark-mode');
+    const currentTheme = document.body.classList.toggle('dark-mode');
+    localStorage.setItem('theme', currentTheme ? 'dark' : 'light');
+    window.location.reload(); // Reload the page to apply the theme
   };
+
+  // Use effect to set the theme on initial load based on local storage
+  useEffect(() => {
+    const savedTheme = localStorage.getItem('theme');
+    if (savedTheme === 'dark') {
+      document.body.classList.add('dark-mode');
+      document.getElementById('theme-switch-checkbox').checked = true;
+    }
+  }, []);
 
   return (
     <label className="theme-switch" htmlFor="theme-switch-checkbox">
